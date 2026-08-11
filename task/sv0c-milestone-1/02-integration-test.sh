81844c121e4f2dad62c23fa665fc768c4bc76e5e
Sasank squatch$ Vishnubhatla <sasank@vishnubhatlas.net>
2026-08-10 21:36:50 -0700

    Bump sv0c to ca4c23c (SML --project impl-method mangling fix); gate impl_methods via --project

--- task/sv0c-milestone-1/02-integration-test.sh
+++ task/sv0c-milestone-1/02-integration-test.sh
@@ -101,7 +101,7 @@ run_case "uc_calculator (enum-dispatched calculator via match)" project "$IT/uc_
 run_case "uc_vec2 (struct + free helper functions)" project "$IT/uc_vec2" 42
 run_case "uc_vec_stack (Vec<i32> as a stack)" project "$IT/uc_vec_stack" 42
 run_case "uc_option_sum (Option-style enum + unwrap_or)" project "$IT/uc_option_sum" 42
-run_case "impl_methods (impl method bodies + self fields + method calls; PC-4c)" one "$IT/impl_methods/main.sv0" 42
+run_case "impl_methods (impl method bodies + self fields + method calls; PC-4c)" project "$IT/impl_methods" 42
 
 echo ""
 echo "results: $PASS passed, $FAIL failed, $TOTAL total"
